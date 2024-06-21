481ee69c0e0af439b6bf2c97ca0f908c93c48065
jefsonoliveira <kaironoliveira16@gmail.com>
2024-06-21 12:44:06 -0300

    feat: Projeto finalzado -Jogo_da_Velha- -ReactJs- -HoraDeCodar-

---
+++ jogo_da_velha/src/components/Square.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+const Square = ({value, onClick}) => {
+  return (
+    <button className="square" onClick={onClick}>{value}</button>
+  );
+};
+
+export default Square;
